3d3fb9f8c1b98113f054ed9d1e86ad9b6bce91af
PamisWang <pamis1116@gmail.com>
2022-02-22 18:20:22 +0800

    test

--- src/index.js
+++ src/index.js
@@ -4,12 +4,70 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+class Welcome extends React.Component
+{
+	render()
+	{
+		return <h1>Hello, {this.props.name}</h1>;
+	}
+}
+
+
+function formatDate(date) {
+	return date.toLocaleDateString();
+  }
+
+// class FormatDate extends React.Component
+// {
+// 	render()
+// 	{
+// 		return date.toLocaleDateString();
+// 	}
+
+// }
+
+
+
+function Comment(props)
+{
+	return (
+		<div className="Comment">
+			<div className="UserInfo">
+				<img className="Avatar"
+					src={props.author.avatarUrl}
+					alt={props.author.name} />
+				<div className="UserInfo-name">
+					{props.author.name}
+				</div>
+			</div>
+			<div className="Comment-text">
+				{props.text}
+			</div>
+			<div className="Comment-date">
+				{formatDate(props.date)}
+			</div>
+		</div>
+	);
+}
+
+const comment = {
+	date: new Date(),
+	text: 'I hope you enjoy learning React!',
+	author: {
+		name: 'Hello Kitty',
+		avatarUrl: 'http://placekitten.com/g/64/64'
+	}
+};
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Comment
+		date={comment.date}
+		text={comment.text}
+		author={comment.author} />,
+	document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
