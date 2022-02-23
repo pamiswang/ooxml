5526f3330dfbbdef36be0b33609acff35d7fbed0
PamisWang <pamis1116@gmail.com>
2022-02-23 23:12:05 +0800

    use react-bootstrap to modify navbar

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -4,6 +4,12 @@ import DocxHome from "../../pages/docx/docx_home"
 import PptxHome from "../../pages/pptx/pptx_home"
 import { Routes, Route, Link } from "react-router-dom";
 
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 class Header extends React.Component
 {
 	render()
@@ -12,40 +18,34 @@ class Header extends React.Component
 			<div className="Header">
 				<h1>選單</h1>
 
-				<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-					<div class="container-fluid">
-
-						<Link class="navbar-brand" to="/">OOXML</Link>
-
-						<div class="collapse navbar-collapse" id="navbarText">
-							<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-								<li class="nav-item">
-
-									<Link class="nav-link active" to="/">Home</Link>
-								</li>
-								<li class="nav-item">
-
-									<Link class="nav-link" to="/docx_home">DOCX Home</Link>
-								</li>
-								<li class="nav-item">
-
-									<Link class="nav-link" to="/pptx_home">PTTX Home</Link>
-								</li>
-							</ul>
-							<span class="navbar-text">
-								Navbar text with an inline element
-							</span>
-						</div>
-					</div>
-				</nav>
-
+				<Navbar collapseOnSelect expand="sm" bg="dark" variant="dark" >
+					<Container>
+						<Navbar.Brand href="/">OOXML</Navbar.Brand>
+						<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+						<Navbar.Collapse id="responsive-navbar-nav">
+							<Nav className="mr-auto">		
+								<Nav.Link href="/">Home</Nav.Link>
+								<Nav.Link href="/docx_home">DOCX Home</Nav.Link>
+								<Nav.Link href="/pptx_home">PTTX Home</Nav.Link>
+						
+								{/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+									<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+									<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+									<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+									<NavDropdown.Divider />
+									<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+								</NavDropdown> */}
+							</Nav>
+						</Navbar.Collapse>
+					</Container>
+				</Navbar>
 
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="docx_home" element={<DocxHome />} />
 					<Route path="pptx_home" element={<PptxHome />} />
 				</Routes>
-			</div>
+			</div >
 		);
 	};
 }
