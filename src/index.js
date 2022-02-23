a0311706e1e655d8626b58f8a7fbb5e8e83f5bab
PamisWang <pamis1116@gmail.com>
2022-02-24 00:51:17 +0800

    basename

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+// import { HashRouter as Router } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 
 
 
 ReactDOM.render(
-	<Router>
+	<Router basename={process.env.PUBLIC_URL}>
 		<App />
 	</Router>,
 	document.getElementById('root')
